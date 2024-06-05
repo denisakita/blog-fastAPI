ac05af12bee6cf902c1de5525d09a42761856a01
Denisa Kita <denisakita7@gmail.com>
2024-06-05 23:04:24 +0200

    add some methods

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"data": "Blog List"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/blog/unpublished")
+async def unpublished():
+    return {"data": "Unpublished"}
+
+
+@app.get("/blog/{id}")
+async def show(id: int):
+    # fetch blog with id = id
+    return {"Blog": id}
+
+
+@app.get("/blog/{id}/comments")
+async def comments(id: int):
+    # fetch comments of blog id = id
+    return {"data": {"1", "2"}}
