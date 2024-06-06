a6f7401bf502e6c08a8ed8f0d4700a9740aafb25
Denisa Kita <denisakita7@gmail.com>
2024-06-06 15:32:42 +0200

    status code

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from blog import models, schemas
+
+from blog import models
 from blog.database import SessionLocal, engine
 from blog.models import Blog as BlogModel
 from blog.schemas import Blog as BlogSchema
@@ -18,10 +19,25 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", response_model=BlogSchema)
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 async def create_blog(request: BlogSchema, db: Session = Depends(get_db)):
     new_blog = BlogModel(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog")
+async def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+async def get_blog_by_id(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Blog does not exist")
+    return blog
